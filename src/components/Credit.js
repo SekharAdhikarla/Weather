09679020bf42667a80c9a070f7852e7427fe7b01
Adhikarla Venkata Sekhar <Adhikarlavenkatasekhar@gmail.com>
2024-09-08 10:03:43 +0530

    initial commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/SekharAdhikarla"
+        > sekhar</a
+      >, 
+    </p>
+  </div>
+  );
+}
